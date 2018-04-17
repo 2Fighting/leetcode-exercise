a40925182946c9e13e85ae2167a3cdd50dbbedcf
2Fighting <591717139@qq.com>
2018-04-17 12:51:09 +0800

    Update Algorthemex.py

--- Algorthemex.py
+++ Algorthemex.py
@@ -1,3 +1,4 @@
+
 #第1题 `两数之和`
 def twosum(self,set,target):
 	d={}
@@ -8,3 +9,12 @@ def twosum(self,set,target):
 		else:
 			d[sub]=i
 		return A
+#第14题·最长子序列·
+def longsub(self,strs):
+	if not strs:
+		return ""
+	for i,regroup in enumerate(zip(*strs)):
+		if len(set(regroup))>1:
+			return strs[0][:i]
+		else:
+			return min（str)
