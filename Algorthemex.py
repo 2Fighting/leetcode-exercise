d4b054ff932602dd8f08da6c67eefb8628189fc7
2Fighting <591717139@qq.com>
2018-04-17 16:02:56 +0800

    Update Algorthemex.py

--- Algorthemex.py
+++ Algorthemex.py
@@ -1,3 +1,4 @@
+
 #第1题 `两数之和`
 def twosum(self,set,target):
 	d={}
@@ -17,3 +18,25 @@ def longsub(self,strs):
 			return strs[0][:i]
 		else:
 			return min（str)
+
+			
+#第695题`dfs and application`
+def dfs(self,grid):
+    #深度优先算法（图论）
+    if 0<= i < m and 0<= j <n and grid[i][j]:
+        grid[i][j]=0
+        return 1+dfs(i+1,j)+dfs(i-1,j)+dfs(i,j+1)+dfs(i,j-1)
+    return 0
+    
+def maxAreaOfIsland(self, grid):
+        maxArea=0
+        m,n = len(grid),len(grid[0])
+        def dfs(i,j):
+            if 0<=i<m and 0<=j<n and grid[i][j]:
+                grid[i][j]=0
+                return 1+dfs(i+1,j)+dfs(i-1,j)+dfs(i,j+1)+dfs(i,j-1)
+            return 0
+        for i in range(m):
+            for j in range(n):
+                maxArea = max(maxArea,dfs(i,j))
+        return maxArea
